3163acd3a7aa5ff8f635eb190b7f90fe33b1ae8d
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-20 22:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',function(){
+	let input=document.getElementById('fname');
+	input.addEventListener('blur',function(){
+		this.value=this.value.toUpperCase();
+	});
+});
